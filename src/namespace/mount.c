a132b01799c54e44c8d27c3a33fd06fc95b0d710
honpery <honpery@gmail.com>
2017-12-05 18:06:29 +0800

    add mount.

---
+++ src/namespace/mount.c
@@ -0,0 +1,36 @@
+/**
+ * mount隔离：CLONE_NEWNS
+ * 
+ * 隔离挂载点
+ */
+#define _GNU_SOURCE
+
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <sched.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#define STACK_SIZE (1024 * 1024)
+
+static char child_stack[STACK_SIZE];
+
+char *const child_args[] = {"/bin/bash", NULL};
+
+int child_main(void *args)
+{
+    printf("child process: %d\n", getpid());
+    execv(child_args[0], child_args);
+    return 1;
+}
+
+int main()
+{
+    printf("main process: %d\n", getpid());
+    int child_pid = clone(child_main, child_stack + STACK_SIZE, CLONE_NEWNS | CLONE_NEWUTS | CLONE_NEWIPC | CLONE_NEWPID | SIGCHLD, NULL);
+    waitpid(child_pid, NULL, 0);
+    printf("exit.\n");
+    return 0;
+}
